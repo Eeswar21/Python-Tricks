54c75989dddc99bf0a17815433378b73c2f1c8b8
Eswar <48028316+Eeswar21@users.noreply.github.com>
2020-01-06 11:38:47 +0000

    control_browser.py
    this is very useful when you want to load websites programmatic.

---
+++ control_browser.py
@@ -0,0 +1,9 @@
+import webbrowser
+
+link1 = "https://www.google.com"
+link2 = "https://www.facebook.com"
+link3 = "https://www.youtube.com"
+
+webbrowser.open_new(link1)
+webbrowser.open_new(link2)
+webbrowser.open_new(link3)
