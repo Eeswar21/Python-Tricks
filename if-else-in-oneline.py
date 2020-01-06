ffd5f3621c7ca15c79313634574754ce22467f80
Eswar <48028316+Eeswar21@users.noreply.github.com>
2020-01-06 14:50:47 +0000

    if-else-in-oneline.py
    if else condition checks in one line code.

---
+++ if-else-in-oneline.py
@@ -0,0 +1,9 @@
+age=20
+status=["not allowed","allowed"][age>18]
+#where [age>18] will return [1] as the condition is true 
+# output : 'allowed'
+
+age = 12
+status=["not allowed","allowed"][age>18]
+#where [age>18] will return [0] as the condition is false
+# output : 'not allowed'
